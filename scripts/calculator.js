dde557914532a3d9da05ed34bbcbd03a1fecb5e5
khalid09dev <mdkhalidhossen@hotmail.com>
2024-03-02 17:04:33 +0600

    nothing to commit, cause i was frustrated

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,55 @@
-prompt('Enter your Input');
+const num1 = prompt('Enter your number:');
+const num2 = prompt('Enter your number:');
+const opr = prompt('Which operation you want to do?');
+
+if(Math.random() < 0.5) {
+    function truthyCalc(num1, num2, calc) {
+        if(calc === 'add') {
+            const add = num1 + num2;
+            return add;
+        }
+        else if(calc === 'subtract') {
+            const subtract = num1 - num2;
+            return subtract;
+        }
+        else if(calc === 'multiply') {
+            const multiply = num1 * num2;
+            return multiply;
+        }
+        else if(calc === 'divide') {
+            const divide = num1 / num2;
+            return divide;
+        }
+    
+        // const calc = 
+        // return add;
+    }
+    const calculate1 = truthyCalc(parseInt(num1), parseInt(num2), opr);
+    const calc = document.getElementById('result')
+    calc.innerText =  calculate1;
+}
+
+else {
+    function faultyCalc(num1, num2, calc) {
+        if(calc === 'add') {
+            const add = num1 - num2;
+            return add;
+        }
+        else if(calc === 'subtract') {
+            const subtract = num1 / num2;
+            return subtract;
+        }
+        else if(calc === 'multiply') {
+            const multiply = num1 + num2;
+            return multiply;
+        }
+        else if(calc === 'divide') {
+            const divide = num1 ** num2;
+            return divide;
+        }
+    }
+
+    const calculate2 = truthyCalc(parseInt(num1), parseInt(num2), opr);
+    const calc = document.getElementById('result')
+    calc.innerText =  calculate2;
+}
